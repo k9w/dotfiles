d7f7f3142241bc0017207dafedeeedcbc1e4f6aa
Kevin Williams <kevin@k9w.org>
2022-04-25 12:36:26 -0700

    Emacs config: Turn on global auto-fill-mode.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -15,18 +15,31 @@
 )
 
 (server-start)
-(setq confirm-kill-emacs 'yes-or-no-p)
 '(fill-column 72)
+(setq-default auto-fill-function 'do-auto-fill)
+(setq confirm-kill-emacs 'yes-or-no-p)
+(setq column-number-mode t)
+(setq tab-bar-show nil)
 (setq indent-tabs-mode nil
     css-indent-offset 2
     js-indent-level 2)
-(setq column-number-mode t)
 (menu-bar-mode -1)
-(setq tab-bar-show nil)
 (split-window-right)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 (toggle-frame-fullscreen)
+
+;; Globally enable follow-mode. This works. But it freezes the
+;; minibuffer. When I try to exit emacs, it doesn't show the exit
+;; confirm until I do Ctrl-G. So I commented it out. Simply specifying
+;; (follow-mode) above only works on the *scratch* buffer. Perhaps
+;; there's a hook that can be called to turn on follow-mode.
+;; https://stackoverflow.com/questions/16048231/how-to-enable-a-non-global-minor-mode-by-default-on-emacs-startup
+;; (define-globalized-minor-mode global-follow-mode follow-mode
+;;   (lambda () (follow-mode 1)))
+
+;; (global-follow-mode 1)
+
 
 ;; Add MELPA package repo to the default ELPA package repo.
 (require 'package)
